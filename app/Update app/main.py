97f2a25238fc522a80c9b46f5c74c26c904e1b27
za4672986-sudo <za4672986@gmail.com>
2026-08-03 17:29:11 +0500

    Include profile and watchlist routers in main.py

--- app/Update app/main.py
+++ app/Update app/main.py
@@ -1,3 +1,7 @@
 from app.database.base import Base
 from app.database.database import engine
 from app.core.exceptions import global_exception_handler
+from app.api.profile import router as profile_router
+from app.api.watchlist import router as watchlist_router
+app.include_router(profile_router)
+app.include_router(watchlist_router)
