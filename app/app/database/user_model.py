b545374f45d808473840c8a8bf2e212843ad5cd2
za4672986-sudo <za4672986@gmail.com>
2026-08-03 17:14:50 +0500

    Create User model for database

---
+++ app/app/database/user_model.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String
+
+from app.database.base import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    username = Column(String(100), unique=True, nullable=False)
+
+    email = Column(String(255), unique=True, nullable=False)
+
+    password = Column(String(255), nullable=False)
