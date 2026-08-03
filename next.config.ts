0a390e45c8675ad424cec8e5d9f13010a271c1b9
za4672986-sudo <za4672986@gmail.com>
2026-08-03 15:21:30 +0500

    Add Next.js configuration file next.config.ts

---
+++ next.config.ts
@@ -0,0 +1,24 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+
+  reactStrictMode: true,
+
+  poweredByHeader: false,
+
+  images: {
+
+    remotePatterns: [
+
+      {
+        protocol: "https",
+        hostname: "**"
+      }
+
+    ]
+
+  }
+
+};
+
+export default nextConfig;
